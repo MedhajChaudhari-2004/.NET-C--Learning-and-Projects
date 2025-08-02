e0275e5899e2515e12d5ff32129433a07a1d5d9f
MedhajChaudhari-2004 <chaudharimedhaj@gmail.com>
2025-08-02 19:13:28 +0530

    2-August-Git-Commit

---
+++ Demo-Project-1/StaticConDemo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_Project_1
+{
+    internal class StaticConDemo
+    {
+
+        static StaticConDemo()
+        {
+            Console.WriteLine("this is a static constructor");
+        }
+
+        public StaticConDemo()
+        {
+            Console.WriteLine("this is a explicit constructor");
+        }
+
+        static void Main()
+
+        {
+            Console.WriteLine("this is a main function");
+            StaticConDemo demo = new StaticConDemo();   
+        }
+    }
+}
